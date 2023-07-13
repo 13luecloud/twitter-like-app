a46083130d66cd7477ef86a1d2599167e289bf29
Donna-Dawn Dagcuta <d.dawnseadd@gmail.com>
2023-07-14 00:33:37 +0800

    Updated Follow  with unit test

--- tests/Unit/FollowUnitTest.php
+++ tests/Unit/FollowUnitTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 
 use App\Http\Repositories\Follow\FollowRepository; 
 use App\Models\User;
+use App\Models\Follow; 
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -14,18 +15,52 @@ class FollowUnitTest extends TestCase
     use RefreshDatabase; 
 
     private $repository;
-    private User $loggedInUser; 
     public function setUp(): void 
     {
         parent::setUp();
         $this->repository = app(FollowRepository::class);
-
-        $user = User::factory(1)->create();
-        $this->loggedInUser = $user; 
     }
 
     public function test_unit_success_follow_user(): void
     {
-        $this->actingAs($this->loggedInUser);
+        $userFollower = User::factory(1)->create();
+        $userFollower = $userFollower->toArray();
+        $userFollowing = User::factory(1)->create();
+        $userFollowing = $userFollowing->toArray();
+
+        $this->assertDatabaseCount('users', 2);
+        $this->assertDatabaseCount('follows', 0);
+
+        $updatedFollowing = $this->repository->followUser($userFollowing[0], $userFollower[0]['account_handle']);
+        $this->assertDatabaseHas('follows', [
+            'follower_id' => $userFollower[0]['account_handle'], 
+            'following_id' => $userFollowing[0]['account_handle']
+        ]);
+
+        $this->assertDatabaseCount('follows', 1);
+    }
+
+    public function test_unit_success_unfollow_user(): void
+    {
+        $userFollower = User::factory(1)->create();
+        $userFollower = $userFollower->toArray();
+        $userFollowing = User::factory(1)->create();
+        $userFollowing = $userFollowing->toArray();
+
+        $followRelationship = Follow::factory(1)->create([
+            'follower_id' => $userFollower[0]['account_handle'], 
+            'following_id' => $userFollowing[0]['account_handle']
+        ]);
+
+        $this->assertDatabaseCount('users', 2);
+        $this->assertDatabaseCount('follows', 1);
+
+        $updatedFollowing = $this->repository->unfollowUser($userFollowing[0], $userFollower[0]['account_handle']);
+        $this->assertDatabaseMissing('follows', [
+            'follower_id' => $userFollower[0]['account_handle'], 
+            'following_id' => $userFollowing[0]['account_handle']
+        ]);
+
+        $this->assertDatabaseCount('follows', 0);
     }
 }
